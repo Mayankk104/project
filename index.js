4ccede265b67993b8e91ce3c855c72d85b4fe11d
Mayank kumar <Mayankk104@gmail.com>
2021-07-24 17:02:49 +0530

    whole number feature

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const utility = require('./folder/utility');
 
-console.log("natural number series")
-
-
+console.log("natural number series.")
 for(let i=0;i<5;i++){
     console.log(utility.increment(i))
 }
+console.log('...')
 
+console.log("whole number series.")
+for(let i=0;i<5;i++){
+    console.log(utility.increment(i-1))
+}
 console.log('...')
